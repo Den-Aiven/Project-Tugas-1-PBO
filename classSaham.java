e8ac72241f8b32e2327d55567bb9cf66b06817cd
Den-Aiven <ivanari432@gmail.com>
2025-04-03 22:11:12 +0800

    Update classSaham.java

--- classSaham.java
+++ classSaham.java
@@ -8,3 +8,19 @@ class Saham {
         this.harga = harga;
     }
 }
+
+class SuratBerhargaNegara {
+    String nama;
+    double bunga;
+    int jangkaWaktu;
+    String tanggalJatuhTempo;
+    double kuotaNasional;
+
+    public SuratBerhargaNegara(String nama, double bunga, int jangkaWaktu, String tanggalJatuhTempo, double kuotaNasional) {
+        this.nama = nama;
+        this.bunga = bunga;
+        this.jangkaWaktu = jangkaWaktu;
+        this.tanggalJatuhTempo = tanggalJatuhTempo;
+        this.kuotaNasional = kuotaNasional;
+    }
+}
