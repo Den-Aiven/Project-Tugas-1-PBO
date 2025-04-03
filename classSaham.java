80c958b8309826dbe35c07e2380aae95f79fc590
sidhiputra <wedaputra15@gmail.com>
2025-04-03 22:13:11 +0800

    Update classSaham.java

--- classSaham.java
+++ classSaham.java
@@ -24,3 +24,13 @@ class SuratBerhargaNegara {
         this.kuotaNasional = kuotaNasional;
     }
 }
+
+class User {
+    String username, password, role;
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+}
