fe8bb31ddb94652890e1586d1ab63b9f58cd88ba
Wedasidhi <wedaputra15@gmail.com>
2025-04-20 23:48:18 +0800

    Initial upload project with package structure (model)

---
+++ src/model/SuratBerhargaNegara.java
@@ -0,0 +1,21 @@
+package model;
+
+public class SuratBerhargaNegara {
+    public String nama;
+    public double bunga;
+    public int jangkaWaktu;
+    public String tanggalJatuhTempo;
+    public double kuotaNasional;
+
+    public SuratBerhargaNegara(String nama, double bunga, int jangkaWaktu, String tanggalJatuhTempo, double kuotaNasional) {
+        this.nama = nama;
+        this.bunga = bunga;
+        this.jangkaWaktu = jangkaWaktu;
+        this.tanggalJatuhTempo = tanggalJatuhTempo;
+        this.kuotaNasional = kuotaNasional;
+    }
+
+    public String toString() {
+        return nama + " - Bunga: " + bunga + "% - Kuota: Rp" + kuotaNasional;
+    }
+}
