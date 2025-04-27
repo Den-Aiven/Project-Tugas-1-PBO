361f9a7b2fbabc427d14c548cf5b1771a2b59bb1
Wedasidhi <wedaputra15@gmail.com>
2025-04-27 12:12:37 +0800

    update package model kelas sbn: perbaiki method

--- src/model/SuratBerhargaNegara.java
+++ src/model/SuratBerhargaNegara.java
@@ -1,11 +1,11 @@
 package model;
 
 public class SuratBerhargaNegara {
-    public String nama;
-    public double bunga;
-    public int jangkaWaktu;
-    public String tanggalJatuhTempo;
-    public double kuotaNasional;
+    private String nama;
+    private double bunga;
+    private int jangkaWaktu;
+    private String tanggalJatuhTempo;
+    private double kuotaNasional;
 
     public SuratBerhargaNegara(String nama, double bunga, int jangkaWaktu, String tanggalJatuhTempo, double kuotaNasional) {
         this.nama = nama;
@@ -15,6 +15,48 @@ public class SuratBerhargaNegara {
         this.kuotaNasional = kuotaNasional;
     }
 
+    // Getter & Setter
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public double getBunga() {
+        return bunga;
+    }
+
+    public void setBunga(double bunga) {
+        this.bunga = bunga;
+    }
+
+    public int getJangkaWaktu() {
+        return jangkaWaktu;
+    }
+
+    public void setJangkaWaktu(int jangkaWaktu) {
+        this.jangkaWaktu = jangkaWaktu;
+    }
+
+    public String getTanggalJatuhTempo() {
+        return tanggalJatuhTempo;
+    }
+
+    public void setTanggalJatuhTempo(String tanggalJatuhTempo) {
+        this.tanggalJatuhTempo = tanggalJatuhTempo;
+    }
+
+    public double getKuotaNasional() {
+        return kuotaNasional;
+    }
+
+    public void setKuotaNasional(double kuotaNasional) {
+        this.kuotaNasional = kuotaNasional;
+    }
+
+    @Override
     public String toString() {
         return nama + " - Bunga: " + bunga + "% - Kuota: Rp" + kuotaNasional;
     }
